c097112dbf14b76bba1d408fcc7c4c02893a1ef2
bekzod90 <114894113+bekzod90@users.noreply.github.com>
2022-11-10 11:27:05 -0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+        public static void main(String[] args) {
+
+            int number1 = 10;
+            int number2 = 5;
+
+            // Using arithmetic operators
+
+            System.out.println("number1 + number2 = " + (number1 + number2));
+
+            System.out.println("number1 - number2 = " + (number1 - number2));
+
+            System.out.println("number1 * number2 = " + (number1 * number2));
+
+            System.out.println("number1 / number2 = " + (number1 / number2));
+
+            System.out.println("number1 % number2 = " + (number1 % number2));
+
+        }
+    }
+
